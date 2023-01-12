e00ba468c3b09a41ccd619a0beb537135336d19f
nepalirakesh <kinarisako@gmail>
2023-01-12 17:30:16 +0545

    code optimized

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,78 +8,81 @@ use Illuminate\Http\Request;
 use App\Models\Author;
 use App\Models\Tag;
 use App\Models\Category;
+use Illuminate\Contracts\View\View;
 
 class PostController extends Controller
 {
-    public function index(){
-        $posts=Post::Paginate(4);
-        return view('blog.post.index',compact('posts'))->with('i', (request()->input('page', 1) - 1) * 5);
-
+    /**
+     * @return View
+     */
+    public function index()
+    {
+        $posts = Post::Paginate(4);
+        return view('blog.post.index', compact('posts'));
     }
 
-    public function create(){
-        
-        $tags=Tag::all();
-        $categories=Category::all();
-        $authors=Author::all();
-
-        return view('blog.post.create',compact('tags','categories','authors'));
+    /**
+     * 
+     */
+    public function create(): View
+    {
+        $tags = Tag::all();
+        $categories = Category::all();
+        $authors = Author::all();
+        return view('blog.post.create', compact('tags', 'categories', 'authors'));
     }
 
-    
-    
-    public function store(PostRequest $request){
 
-
+    /**
+     * @param PostRequest $request
+     * @return \Illuminate\Routing\Redirector|\Illuminate\Http\RedirectResponse`
+     */
+    public function store(PostRequest $request)
+    {
         $data = $request->validated();
+
         $post = new Post;
-  
         $post->author_id = $data['author'];
         $post->title = $data['title'];
         $post->description = $data['description'];
         $post->content = $data['content'];
         $post->category_id = $data['category'];
 
-        if($request->hasFile('image')){
-            $file=$request->file('image');
-            $filename=$file->getClientOriginalName();
-            $file->storeAs('public/images/',$filename);
-            $post->image=$filename;
+        if ($request->hasFile('image')) {
+            $file = $request->file('image');
+            $filename = $file->getClientOriginalName();
+            $file->storeAs('public/images/', $filename);
+            $post->image = $filename;
         }
-   
+
         $post->save();
         $post->tag()->attach($request->tags);
-   
-        return redirect()->route('post.index')->with('success','post added successfully'); 
-    
-    }    
 
-    public function show(Post $post){
-
-        return view('blog.post.show',compact('post'));
+        return redirect()->route('post.index')->with('success', 'post added successfully');
     }
 
-
-
-
-    public function edit(Post $post){
-
-        $categories=Category::all();
-        $tags=Tag::all();
-        $authors=Author::all();
-        return view('blog.post.edit',compact('post','categories','tags','authors'));
-
+    public function show(Post $post)
+    {
+        return view('blog.post.show', compact('post'));
     }
 
-    public function update(Post $post, PostRequest $request){
+    public function edit(Post $post)
+    {
+        $categories = Category::all();
+        $tags = Tag::all();
+        $authors = Author::all();
+        return view('blog.post.edit', compact('post', 'categories', 'tags', 'authors'));
+    }
 
+    public function update(Post $post, PostRequest $request)
+    {
         $data = $request->validated();
 
-        if($request->hasFile('image')){
-            $file=$request->file('image');
-            $filename=$file->getClientOriginalName();
-            $file->storeAs('public/images/',$filename);
-            $post->image=$filename;
+        if ($request->hasFile('image')) {
+            $file = $request->file('image');
+            $filename = $file->getClientOriginalName();
+            $file->storeAs('public/images/', $filename);
+            $post->image = $filename;
         }
 
         $post->author_id = $data['author'];
@@ -87,24 +90,15 @@ class PostController extends Controller
         $post->description = $data['description'];
         $post->content = $data['content'];
         $post->category_id = $data['category'];
-
-
         $post->save();
         $post->tag()->sync($request->tags);
-        // dd($request->all());
-       
-        return redirect()->route('post.index')->with('update','post updated successfully');
 
-        }
+        return redirect()->route('post.index')->with('update', 'post updated successfully');
+    }
 
-
-
-    
-
-    public function delete(Post $post){
-
+    public function delete(Post $post)
+    {
         $post->delete();
-        return redirect()->route('post.index')->with('delete','post deleted successfully');
-
+        return redirect()->route('post.index')->with('delete', 'post deleted successfully');
     }
 }
