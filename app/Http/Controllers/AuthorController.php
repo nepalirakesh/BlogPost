cd5e994b4913c2f61c1b7b66fbaa1718d819c23b
Sunita-Shrestha <sthasunita987@gmail.com>
2023-01-17 09:44:32 +0545

    ImageUpload trait is applied in AuthorController

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -5,42 +5,44 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Author;
 use App\Http\Requests\AuthorRequest;
+use App\Traits\ImageUpload;
+
 
 class AuthorController extends Controller
 {
+    use ImageUpload;
+
     //For Index or Viewing
     public function show()
-    {   
+    {
         $authors = Author::paginate(4);
-        
+
         return view('blog/author/index',compact('authors'))->with('i', (request()->input('page', 1) - 1) * 5);
     }
-    
+
 
     //For creating a new author
     public function store(AuthorRequest $request)
-    {   
+    {
 
-        $validatedData = $request->validated();  
+        $validatedData = $request->validated();
         $author = new Author();
         $author->name = $request->name;
        // Image exists or not
        if($request->file('image'))
        {
-        $file = $request->file('image');
-        $filename = $file->getClientOriginalName();
-        $file->storeAs(('public/images'), $filename);
-        $author->image = $filename;
+        $author->image = $this->uploadImage($request->file('image'));
+
        }
         $author->description = $request->description;
         $author->save();
-        
+
         return redirect('author')->with('success','Author Created Successfully');
     }
 
 
     //For viewing edit page
-    
+
      public function edit($id)
      {
         $author = Author::find($id);
@@ -52,7 +54,7 @@ class AuthorController extends Controller
      //For updating author information
 
      public function update(Request $request, $id)
-    {   
+    {
         $request->validate([
             'name' =>'required|max:255',
             'description' =>'required|max:255',
@@ -61,15 +63,13 @@ class AuthorController extends Controller
         $author = Author::find($id);
         $author->name = $request->name;
 
-        //Images upload 
+        //Images upload
         if($request->file('image'))
        {
-        $file = $request->file('image');
-        $filename = $file->getClientOriginalName();
-        $file->storeAs(('public/images'), $filename);
-        $author->image = $filename;
+        $author->image = $this->uploadImage($request->file('image'));
+
        }
-       
+
 
         $author->description = $request->description;
         $author->save();
